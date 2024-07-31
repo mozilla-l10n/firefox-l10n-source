28b151cb721d5f057f2dc753f378791786e92053
Eemeli Aro <eemeli@mozilla.com>
2024-07-31 12:10:42 +0300

    Move _configs/config.json -> .github/update-config.json

--- .github/scripts/prune.py
+++ .github/scripts/prune.py
@@ -108,7 +108,7 @@ if __name__ == "__main__":
     parser = ArgumentParser(prog=prog, description=__doc__)
     args = parser.parse_args()
 
-    config_file = join("_configs", "config.json")
+    config_file = join(".github", "update-config.json")
     with open(config_file) as f:
         cfg_automation = json.load(f)
 
--- .github/scripts/update.py
+++ .github/scripts/update.py
@@ -173,7 +173,7 @@ def write_commit_msg(args, new_files: int, updated_files: int):
 
 
 if __name__ == "__main__":
-    config_file = join("_configs", "config.json")
+    config_file = join(".github", "update-config.json")
     with open(config_file) as f:
         cfg_automation = json.load(f)
 
